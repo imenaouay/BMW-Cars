beac7534ca768c803f02b49db72daab4f4827ece
Imen away <imen2way@gmail.com>
2024-08-15 10:30:01 +0100

    cars version 1 landing page

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// toggle menu button
+function toggleMenu(){
+    const menu = document.querySelector('.menu');
+    const nav = document.querySelector('.nav');
+    menu.classList.toggle('active');
+    nav.classList.toggle('active');
+}
+
+//change the background video when click on the gallery images
+
+function changeVideo(name){
+    const bgVideoList = document.querySelectorAll('.bg-video');
+    const trailers = document.querySelectorAll('.trailer');
+    const models = document.querySelectorAll('.model');
+
+
+    bgVideoList.forEach(video => {
+        video.classList.remove('active');
+        if (video.classList.contains(name)){
+            video.classList.add('active');
+        }
+    });
+}
+
